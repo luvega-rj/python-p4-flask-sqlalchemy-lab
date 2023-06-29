e67c1c6bf219a5319d8b4134221c8c3cce03d998
luvega-rj <luvegaronny@gmail.com>
2023-06-29 15:22:35 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python3
-
 from flask import Flask, make_response
 from flask_migrate import Migrate
-
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 migrate = Migrate(app, db)
-
 db.init_app(app)
-
 @app.route('/')
 def home():
     return '<h1>Zoo app</h1>'
-
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
-
+    animal = Animal.query.get(id)
+    zookeeper = Zookeeper.query.get(id)
+    enclosure = Enclosure.query.get(id)
+    return f'<ul>ID: {animal.id}</ul><ul>Name: {animal.name}</ul><ul>Species: {animal.species}</ul><ul>Zookeeper: {zookeeper.name}</ul><ul>Enclosure: {enclosure.environment}</ul></ul>'
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
-
+    zookeeper = Zookeeper.query.get(id)
+    return f'<ul>ID: {zookeeper.id}</ul><ul>Name: {zookeeper.name}</ul><ul>Birthday: {zookeeper.birthday}</ul>'
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
-
+    enclosure = Enclosure.query.get(id)
+    return f'<ul>ID: {enclosure.id}</ul><ul>Environment: {enclosure.environment}</ul><ul>Open_to_visitors: {enclosure.open_to_visitors}</ul>'
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,23 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
-
 db = SQLAlchemy(metadata=metadata)
-
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
-
     id = db.Column(db.Integer, primary_key=True)
-
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
-
     id = db.Column(db.Integer, primary_key=True)
-
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
 class Animal(db.Model):
     __tablename__ = 'animals'
-
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
